e271419b6d7ab84fc5bc711ff7b48f987a34841d
forza111 <nikitos.ionkin@yandex.ru>
2020-06-16 23:09:18 +0300

    Создание формы входа на сайт

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,flash,redirect
 from app import app
 from app.forms import LoginForm
 
@@ -24,5 +24,17 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    '''аргумент methods - сообщение FLASK,что эта
+#функция просмотра принимает запросы GET и POST(по умолчанию только GET)
+#GET запросы - это те, что возвращают информацию клиенту(в этом случае веб браузер
+#запросы POST обычно исп. когда браузер передает данные формы на сервер'''
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me {}'.format(
+            form.username.data, form.remember_me.data))
+        '''flash - фласк сохраняет сообщение, но на веб серверах не
+        будут появляться сообщения'''
+        return redirect('/index')
+    '''функция указывает веб браузеру клиента автоматически
+    перейти на другую страницу, указанную в качестве аргумента(/index)'''
     return render_template('login.html',title = 'Sign In',form = form)
