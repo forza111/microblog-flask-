ed776e516a6b8980834ba1f6875b5515fac68a76
forza111 <nikitos.ionkin@yandex.ru>
2020-06-17 08:31:49 +0300

    Расширение таблицы БД. Добавление нового класса Post в models.py. Новая миграция БД

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -10,6 +11,28 @@ class User(db.Model):
     тип поля в качестве аргумента + необязательный аргументы, которые позволяют указать
     какие поля уникальны и проиндексированы'''
 
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    '''retationship - высокоуровневое представление о взаимоотношениях между users и posts.
+
+    Для отношения один ко многим(напр. один пользователь и много сообщений) поле relationship 
+    определяется обычно на стороне "один" и используется как удобный способ получить доступ к 
+    "многим" 
+
+    Первый аргумент - Post указывает класс, который представляет сторону отношений "много"
+    backref - определяет имя поля, которое будет добавлено
+    lazy - определяет как будет выполняться запрос БД для связи'''
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     '''сообщает Python,как печатать объекты этого класса, что будет полезно для отладки'''
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    '''Поле timestamp будет  проиндексировано(сообщения в хронологическом порядке)'''
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    '''user_id - внешний ключ для user.id - значит оно ссылается на значение id из таблицы users'''
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
