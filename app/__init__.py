eb78579d607b8e9532bbe044e98c2aae06e1c825
forza111 <nikitos.ionkin@yandex.ru>
2020-06-18 09:17:09 +0300

    создание входа пользователя в систему

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 '''name -переменная, переданная в класс Flask, является предопределенной переменной
@@ -10,6 +11,9 @@ app.config.from_object(Config)
 '''Flask читает фаил конфигурации и применяет'''
 db = SQLAlchemy(app)#объект db представляет БД
 migrate = Migrate(app,db)#объект, который представит механизм миграции
-
+login = LoginManager(app)
+login.login_view = 'login'
+'''значение login-именем функции(конечной точкой) для входа в систему. Другими словами имя,которое
+вы будете использовать в вызове url_for, чтобы получить url'''
 
 from app import routes,models
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template,flash,redirect,url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user,login_user,logout_user,login_required
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required#функция становится защищенной и не разрешает доступ к пользователям, которые не аутентифицированны
 def index():
     user = {'username': 'Эльдар Рязанов'}
     posts = [
@@ -24,17 +27,30 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    '''аргумент methods - сообщение FLASK,что эта
-#функция просмотра принимает запросы GET и POST(по умолчанию только GET)
-#GET запросы - это те, что возвращают информацию клиенту(в этом случае веб браузер
-#запросы POST обычно исп. когда браузер передает данные формы на сервер'''
+    if current_user.is_authenticated:#current_us-Значение этой переменной может быть пользовательским
+        #объектом из БД (который Flask-Login читает через обратный вызов загрузчика пользователя,
+        #представленный выше), или специальный анонимный пользовательский объект, если пользователь
+        # еще не входил в систему
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me {}'.format(
-            form.username.data, form.remember_me.data))
-        '''flash - фласк сохраняет сообщение, но на веб серверах не
-        будут появляться сообщения'''
+        user = User.query.filter_by(username=form.username.data).first
+        '''результат функции filter_by- это запрос, который включает только объекты, у которых есть совпадающее
+        имя польователя.Поскольку известно, что будет только один или нулевой результат,я завершу запрос,
+        вызвав first(), который вернет объект пользователя,если он существует или None'''
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')#flash - фласк сохраняет сообщение, но на веб серверах не
+            # будут появляться сообщения
+            return redirect(url_for('login'))#функция указывает веб браузеру клиента автоматически
+            # #перейти на другую страницу, указанную в качестве аргумента(/login)
+        login_user(user,remember=form.remember_me.data)
+        '''функция будет регестрировать пользователя во время входа в систему, поэтому это означает, что
+        на любых будущих страницах, к которым пользователь переходит, будет установлена переменная 
+        current_user для этого пользователя'''
         return redirect(url_for('index'))
-    '''функция указывает веб браузеру клиента автоматически
-    перейти на другую страницу, указанную в качестве аргумента(/index)'''
     return render_template('login.html',title = 'Sign In',form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
